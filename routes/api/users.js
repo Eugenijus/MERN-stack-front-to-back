15d93a7f81b44d70b3c2ffa652f3d60357a91f41
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2018-10-16 17:11:37 +0300

    011 Email & Password Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,7 @@ const User = require('../../models/User');
 // @access Public
 router.get('/test', (req, res) => res.json({msg: 'Users works!'}));
 
-// @route GET api/users/register
+// @route POST api/users/register
 // @desc  Register
 // @access Public
 router.post('/register', (req, res) => {
@@ -44,6 +44,33 @@ router.post('/register', (req, res) => {
           })
         });
       }
+    });
+});
+
+// @route POST api/users/login
+// @desc  Login User / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  // Find user by emal
+  User.findOne({email})
+    .then(user => {
+      // Check for user
+      if(!user) {
+        return res.status(400).json({email: 'User not found!'});
+      }
+
+      // Check the password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            res.json({msg: 'Success'});
+          } else {
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        });
     });
 });
 
