f54af17a2c1e4afb549dd53c028fb7af995f07a1
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2018-10-18 13:48:28 +0300

    027 Post Like & Unlike Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -90,4 +90,60 @@ router.delete('/:id',
   }
 );
 
+// @route POST api/posts/like/:id
+// @desc  Like post by id
+// @access Private
+router.post('/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+              return res.status(400).json({ alreadLiked: 'User have already liked this post' });
+            }
+
+            // Add user id to likes array
+            post.likes.unshift({ user: req.user.id });
+
+            post.save().then(post => res.json(post));
+          })
+          .catch(err => res.status(404).json({ postNotFound: 'No post found with that ID'}));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route POST api/posts/unlike/:id
+// @desc  Unlike post by id
+// @access Private
+router.post('/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+              return res.status(400).json({ alreadLiked: 'User have not yet liked this post' });
+            }
+
+            // Get remove index
+            const removeIndex = post.likes
+              .map(item => item.user.toString())
+              .indexOf(req.user.id);
+
+            // Remove user id from likes array
+            post.likes.splice(removeIndex, 1);
+
+            // Save
+            post.save().then(post => res.json(post));
+          })
+          .catch(err => res.status(404).json({ postNotFound: 'No post found with that ID'}));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
