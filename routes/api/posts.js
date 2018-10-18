292ccc1537ade5e8037d0d7385b435efba266e04
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2018-10-18 13:35:28 +0300

    026 Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,8 @@ const passport = require('passport');
 
 // Post Model
 const Post = require('../../models/Post');
+// Profile Model
+const Profile = require('../../models/Profile');
 
 // Validation
 const validatePostInput = require('../../validation/post');
@@ -13,6 +15,29 @@ const validatePostInput = require('../../validation/post');
 // @desc  Tests post route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: 'Posts works!'}));
+
+// @route GET api/posts
+// @desc  Get all posts
+// @access Public
+router.get('/',
+  (req, res) => {
+    Post.find()
+      .sort({ date: -1 })
+      .then(posts => res.json(posts))
+      .catch(err => res.status(404).json({noPostsFound: 'No posts found'}));
+  }
+);
+
+// @route GET api/posts/:id
+// @desc  Get post by id
+// @access Public
+router.get('/:id',
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(posts => res.json(posts))
+      .catch(err => res.status(404).json({noPostFound: 'No post found with that ID'}));
+  }
+);
 
 // @route POST api/posts
 // @desc  Create post
@@ -37,6 +62,30 @@ router.post(
 
     newPost.save() 
       .then(post => res.json(post))
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route DELETE api/posts/:id
+// @desc  Delete post by id
+// @access Private
+router.delete('/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            // Check for post owner
+            if(post.user.toString() !== req.user.id) {
+              return res.status(401).json({ notAuthorized: 'User not authorized!' });
+            }
+
+            // Delete
+            post.remove().then(() => res.json({ succes: 'true', post }));
+          })
+          .catch(err => res.status(404).json({ postNotFound: 'No post found with that ID'}));
+      })
       .catch(err => res.status(404).json(err));
   }
 );
