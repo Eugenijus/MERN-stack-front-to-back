bf6c69fe14c0b0b9719eb415f5014491c6b4419e
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2018-10-29 18:36:46 +0200

    040 Redux Action and Reducer Workflow Example

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor() {
@@ -31,17 +34,22 @@ class Register extends Component {
       password2: this.state.password2,
     };
 
+    this.props.registerUser(newUser);
+
     // Register our user
-    axios.post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
+    // axios.post('/api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
     const { errors } = this.state;
 
+    const { user } = this.props.auth;
+
     return (
       <div className="register">
+        { user ? user.name : null }
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -115,4 +123,15 @@ class Register extends Component {
   }
 }
 
-export default Register;
+Register.PropTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+
+// Putting *auth* state inside of property called *auth*
+// *auth* state comes from root reducer
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {registerUser} )(Register);
