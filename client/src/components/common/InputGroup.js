45cf88f74f9565a2d540de9d60a7a18e70d7253c
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2018-11-01 21:55:12 +0200

    050 Form Field Components

--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -2,19 +2,22 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextFieldGroup = ({
+const InputGroup = ({
   name,
   placeholder,
   value,
-  label,
+  icon,
   error,
-  info,
   type,
   onChange,
-  disabled
 }) => {
   return (
-    <div className="form-group">
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon} />
+        </span>
+      </div>
       <input
         type={type}
         className={classnames('form-control form-control-lg', {
@@ -24,27 +27,24 @@ const TextFieldGroup = ({
         name={name}
         value={value}
         onChange={onChange}
-        disabled={disabled}
       />
-      {info && <small className="form-text text-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div> }
     </div>
   );
 };
 
-TextFieldGroup.propTypes = {
+InputGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
-  info: PropTypes.string,
+  icon: PropTypes.string,
   error: PropTypes.string,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string,
 };
 
-TextFieldGroup.defaultProps = {
+InputGroup.defaultProps = {
   type: 'text'
 };
 
-export default TextFieldGroup;
+export default InputGroup;
