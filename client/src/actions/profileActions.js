71d0820f6f7977bee0557e51590eb442a2271ef6
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2018-11-04 12:00:07 +0200

    060 Profiles Component and getProfiles Action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,7 +5,8 @@ import {
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
-  SET_CURRENT_USER
+  SET_CURRENT_USER,
+  GET_PROFILES
 } from './types';
 
 // Get current profile
@@ -21,6 +22,24 @@ export const getCurrentProfile = () => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_PROFILE,
+        payload: {},
+      })
+    );
+};
+
+// Get profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios.get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data,
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
         payload: {},
       })
     );
