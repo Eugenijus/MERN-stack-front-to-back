af83c19c2d2fca32ec5a47a360707a21d147e46f
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2018-11-05 23:31:42 +0200

    070 Delete Like and Unlike Posts

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,6 +5,7 @@ import {
   GET_POSTS,
   GET_ERRORS,
   POST_LOADING,
+  DELETE_POST,
 } from './types';
 
 // Add Post
@@ -38,6 +39,52 @@ export const getPosts = () => dispatch => {
       dispatch({
         type: GET_POSTS,
         payload: null,
+      })
+    );
+};
+
+// Delete Post
+export const deletePost = id => dispatch => {
+  if(window.confirm('Are you sure? This can NOT be undone!')) {
+    axios
+      .delete(`/api/posts/${id}`)
+      .then(res =>
+        dispatch({
+          type: DELETE_POST,
+          payload: id
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
     );
 };
